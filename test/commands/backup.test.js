41ac506811532103d3ee45c7efba8bef5e506724
中村 恵介 <keisuke.nakamura@bizreach.co.jp>
2018-11-26 10:07:16 +0900

    Update command test

--- test/commands/backup.test.js
+++ test/commands/backup.test.js
@@ -4,22 +4,21 @@ const sinon = require('sinon');
 // mock
 const backup = require('../../src/executer/backup');
 
-describe('backup', () => {
+const region = 'test region';
+const userPoolId = 'test userPoolId';
+
+describe('backup command test', () => {
   // stub
   const stub = sinon.stub(backup, 'main');
   stub.returns(new Promise(resolve => resolve()));
 
-  test.stdout().command(['backup']).it('runs backup', ctx => {
-    expect(ctx.stdout).to.contain(`target region`);
-    expect(ctx.stdout).to.contain(`target userPoolId`);
-  });
-
-  const region = 'test region';
-  const userPoolId = 'test userPoolId';
   test.stdout().
     command(['backup', '--region', region, '--userPoolId', userPoolId]).
     it('runs backup parameter test', ctx => {
       expect(ctx.stdout).to.contain(`target region ${region}`);
       expect(ctx.stdout).to.contain(`target userPoolId ${userPoolId}`);
     });
+  it('runs backup parameter fail test', () => {
+    test.stderr().command(['backup', '--region', region]).exit(2);
+  });
 });
